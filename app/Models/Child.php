92d883b55de760a0ee3997cd8513064f12e1ebf0
bainhaboraya <bainhaboraya1@gmail.com>
2024-06-23 14:41:29 +0300

    aaa

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -2,11 +2,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Parents; // Make sure to import the Parents model
+use App\Models\Parents;
 
 class Child extends Model
 {
-    
+
     protected $fillable = [
         'parent_id',
         'full_name',
@@ -15,7 +15,7 @@ class Child extends Model
         'gender',
         'photo',
         'current_residence',
-        // Add any other attributes that you want to allow mass assignment for
+        
     ];
     public function parent(){
         return $this->belongsTo(Parents::class, 'parent_id');
@@ -25,7 +25,7 @@ class Child extends Model
     {
         return $this->hasOne(Application::class);
     }
-    
+
     public function enrollments()
     {
         return $this->hasMany(Enrollment::class);
