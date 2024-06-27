4871477aec312a426c98b1e87cb351c2848371f2
bainhaboraya <bainhaboraya1@gmail.com>
2024-06-27 15:09:56 +0300

    editing

--- app/Http/Resources/ParentResource.php
+++ app/Http/Resources/ParentResource.php
@@ -16,6 +16,7 @@ class ParentResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'user_id' => $this->user_id, 
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'educational_qualification' => $this->educational_qualification,
